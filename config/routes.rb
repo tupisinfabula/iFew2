712966a057a4aea67edf105f43ac6df9d5456b70
Seila <seila.tupone@gmail.com>
2016-09-10 09:49:00 +0200

    Added scaffold todo_list and item

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
+  resources :todo_lists do
+    resources :todo_items
+  end
   devise_for :users
   resources :workouts
+
   root 'workouts#index'
-  get '/auth/spotify/callback', to: 'users#spotify'
+  #get '/auth/spotify/callback', to: 'users#spotify'
 end
